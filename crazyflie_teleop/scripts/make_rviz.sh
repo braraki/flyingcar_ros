fa29ea172361854c90559149b94dfa640ab983d0
Sarah Pohorecky <spohorec@mit.edu>
2016-08-04 13:08:16 -0400

    Worked on multiple path-following display in RViz

--- crazyflie_teleop/scripts/make_rviz.sh
+++ crazyflie_teleop/scripts/make_rviz.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-name=$1
-
 cd $(dirname $0)
 cd ../launch
 
 rm -f "crazyflie_pos.rviz"
 
-sed "s/ROBOT_NAME/$name/g" "pos_template.rviz" >> "crazyflie_pos.rviz"
+cat ./rviz_template/rviz_head >> "crazyflie_pos.rviz"
+echo -e "\n" >> "crazyflie_pos.rviz"
+
+while [ $# -ne 0 ]; do
+	sed "s/ROBOT_NAME/$1/g" ./rviz_template/rviz_robot_content >> "crazyflie_pos.rviz"
+	echo -e "\n" >> "crazyflie_pos.rviz"
+	shift
+done
+
+cat ./rviz_template/rviz_foot >> "crazyflie_pos.rviz"
+
+#sed "s/ROBOT_NAME/$1/g" pos_template.rviz >> "crazyflie_pos.rviz"
 
 exit 0
